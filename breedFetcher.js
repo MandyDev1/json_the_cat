4f3ee3b83ce3914738dcaae12e495a4cfdbd538e
Mandy Liu <manling512@gmail.com>
2023-07-15 19:03:54 +0000

    Refactored the code & added some test code

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,24 +1,22 @@
 const request = require('request');
 
-// Get the breed name from command-line arguments
-const breedName = process.argv[2];
+const fetchBreedDescription = function(breedName, callback) {
+  const apiUrl = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
 
-// API endpoint URL with the breed name as a query parameter
-const apiUrl = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
-
-request(apiUrl, (error, response, body) => {
-  if (error) {
-    console.error('Request error:', error);
-  } else {
-    // Convert the JSON string into an object
-    const data = JSON.parse(body);
-
-    if (data.length === 0) {
-      console.log(`Breed '${breedName}' not found.`);
+  request(apiUrl, (error, response, body) => {
+    if (error) {
+      callback(error, null);
     } else {
-      // Access the first entry in the data array and print the description
-      const description = data[0].description;
-      console.log(description);
+      const data = JSON.parse(body);
+
+      if (data.length === 0 || !body) {
+        callback(new Error(`Breed '${breedName}' not found.`), null);
+      } else {
+        const description = data[0].description;
+        callback(null, description);
+      }
     }
-  }
-});
+  });
+};
+
+module.exports = {fetchBreedDescription};
